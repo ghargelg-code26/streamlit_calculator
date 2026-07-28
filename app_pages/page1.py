2731a75e41f937964572a740083824522ed195f7
ghargelg-code26 <ghargelg@gmail.com>
2026-07-28 23:22:09 +0100

    skeleton of app with three pages with one as calculator

---
+++ app_pages/page1.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def page1_body():
+    st.write("this is page one!")
---
+++ calculator.py
@@ -0,0 +1,4 @@
+# Importing the Streamlit Library
+import stramlit as st
+
+from app_pages.multi_page import Multipage
