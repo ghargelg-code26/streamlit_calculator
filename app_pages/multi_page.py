752aef25871b5a8f1cc44064e7c001824c896ad9
ghargelg-code26 <ghargelg@gmail.com>
2026-07-28 23:44:13 +0100

    pushing running app developement files  after successfull run on local host

--- app_pages/multi_page.py
+++ app_pages/multi_page.py
@@ -8,7 +8,7 @@ class MultiPage:
         
 
         st.set_page_config(
-            page_title=self.app_name
+            page_title=self.app_name,
             page_icon=":computer:"
         )
 
@@ -19,4 +19,3 @@ class MultiPage:
         st.title(self.app_name)
         page = st.sidebar.radio("Menu", self.pages, format_func=lambda page:page["title"])
         page["function"]()
-        
--- maultipage_app_with_calculator.py
+++ maultipage_app_with_calculator.py
@@ -1,12 +1,14 @@
 # Importing the Streamlit Library
-import stramlit as st
-
+import streamlit as st
+#importing the MultiPage class from app_pages.multi_page module
 from app_pages.multi_page import MultiPage
-
+ # Importing the calculator_body function from app_pages.page_calculator module
+from app_pages.page_calculator import calculator_body 
 from app_pages.page1 import page1_body
 from app_pages.page2 import page2_body
-
-app = MultiPage(app_name="This is my first app in Streamlit!")
+# Declaring the app name
+app = MultiPage(app_name="app with calculator")
+# Creating an instance of the MultiPage class with the app name "Calculator App"
 
 app.add_page("Page 1", page1_body)
 app.add_page("Page 2", page2_body)
